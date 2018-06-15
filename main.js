ec144873fa3321939ca791021c76fb3f3cabd4ec
eliascerutti <33574864+eliascerutti@users.noreply.github.com>
2018-06-15 23:00:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+function add(a, b){
+    var res = a+b;
+    return res;
+}
+
+function sub(a, b){
+    var res = a-b;
+    return res;
+}
+
+function mul(a, b){
+    var res = a*b;
+    return res;
+}
+
+function div(a, b){
+    var res = a/b;
+    return res;
+}
+
+ console.log(add (mul(1,2), div(3,1)));
+ console.log(sub (add(1,2), sub(3,1)));
+ console.log(mul (sub(17,16), div(350,5)));
+ console.log(div (mul(8,7), mul(2,4)));
+ console.log(add (mul(18,3), sub(35,6)));
+
+ 
+ 
